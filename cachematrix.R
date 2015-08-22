4e58607fec3962a80145ec88efdc3c816af3289d
letifer <sanguispugnus@gmail.com>
2015-08-22 22:59:26 +0300

    solution added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions to calculate and cash matrix inverse
 
-## Write a short comment describing this function
+## turns matrix into an object with cache slots
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsol <- function(solve) m <<- solve
+  getsol <- function() m
+  list(set = set, get = get, setsol = setsol, getsol = getsol)
 }
 
 
-## Write a short comment describing this function
+## calculates the inverse of the cached-matrix-object or, if available,
+## return the cached inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsol()
+  if(!is.null(m)) {
+    print('cached data available')
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsol(m)
+  m
 }
+
+
